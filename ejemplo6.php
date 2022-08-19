8c3ee1ab5699c804db2f8afa68a1859c83aef22f
MATEO LOPEZ GARCIA <1193116825@ACDCECAR.LOCAL>
2022-08-19 08:53:23 -0500

    Se agregaron dos ejemplos en clase.

---
+++ ejemplo6.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+$palabra = array["Mango","Patilla","Pera","Manzana","Mandarina"];
+
+
+for ($i=0; $i < count($palabra) ; $i++) { 
+
+    print $palabra[$i]
+
+    
+}
+
+
+
+
+
+
+
+
+
+?>
